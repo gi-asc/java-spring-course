3cefbbc20dd3877ddd13834575adf9a26682aef3
Giovana Carvalho <giovanaaraujo3003@gmail.com>
2022-03-16 16:21:06 -0300

    feat: implements findAll and findById on user

--- src/main/java/com/udemy/curso/resources/UserResourse.java
+++ src/main/java/com/udemy/curso/resources/UserResourse.java
@@ -1,19 +1,33 @@
 package com.udemy.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.curso.entities.User;
+import com.udemy.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResourse {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1, "Maria", "maria@teste.com", "9999-9999", "teste124");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
