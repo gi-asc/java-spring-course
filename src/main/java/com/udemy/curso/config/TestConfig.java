29a062f1e07972590adfe6c20b893417d615fcc1
Giovana Carvalho <giovanaaraujo3003@gmail.com>
2022-03-21 17:06:47 -0300

    feat: implements orderItems

--- src/main/java/com/udemy/curso/config/TestConfig.java
+++ src/main/java/com/udemy/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.udemy.curso.entities.Category;
 import com.udemy.curso.entities.Order;
+import com.udemy.curso.entities.OrderItem;
 import com.udemy.curso.entities.Product;
 import com.udemy.curso.entities.User;
 import com.udemy.curso.entities.enums.OrderStatus;
 import com.udemy.curso.repositories.CategoryRepository;
+import com.udemy.curso.repositories.OrderItemRepository;
 import com.udemy.curso.repositories.OrderRepository;
 import com.udemy.curso.repositories.ProductRepository;
 import com.udemy.curso.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -62,5 +67,12 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
--- src/main/java/com/udemy/curso/repositories/ProductRepository.java
+++ src/main/java/com/udemy/curso/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.udemy.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.udemy.curso.entities.Category;
 import com.udemy.curso.entities.Product;
 
 
