76126ff6c232849f01fbcb8222633d65d8b38d29
Giovana Carvalho <giovanaaraujo3003@gmail.com>
2022-03-25 13:01:38 -0300

    feat: create payment entity

--- src/main/java/com/udemy/curso/config/TestConfig.java
+++ src/main/java/com/udemy/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy.curso.entities.Category;
 import com.udemy.curso.entities.Order;
 import com.udemy.curso.entities.OrderItem;
+import com.udemy.curso.entities.Payment;
 import com.udemy.curso.entities.Product;
 import com.udemy.curso.entities.User;
 import com.udemy.curso.entities.enums.OrderStatus;
@@ -74,5 +75,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
